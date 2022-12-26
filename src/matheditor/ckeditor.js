bfe94418d1852b5dde4d415241348a4436bce732
ashutosh <ashutosh.dangi78@gmail.com>
2022-12-26 23:12:18 +0530

    plugin edit

--- src/matheditor/ckeditor.js
+++ src/matheditor/ckeditor.js
@@ -32,8 +32,9 @@ import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformatio
 import Mathtype from '@wiris/mathtype-ckeditor5';
 import Font from '../font/font';
 import FontFamily from '../font/fontfamily';
-import ImageUploadPlugin from '@ckeditor/ckeditor5-image/src/imageupload'
-import  CloudServicesPlugin  from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
+import  CloudServices  from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
+
+
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
@@ -64,8 +65,7 @@ ClassicEditor.builtinPlugins = [
 	Mathtype,
 	Font,
 	FontFamily,
-	ImageUploadPlugin,
-	CloudServicesPlugin
+	CloudServices
 ];
 
 // Editor configuration.
